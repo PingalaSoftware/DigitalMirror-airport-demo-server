abb9b0eb77c98cacecb660d03f344de65cf3e441
aaronchellaya <aaron@pingalasoftware.com>
2022-12-28 12:20:13 +0530

    changes done

--- app.js
+++ app.js
@@ -7,68 +7,68 @@ const cors = require("cors");
 app.use(cors());
 var fs = require("fs");
 
-// app.get('/', (req, res, next) => {
-//   console.log(__dirname)
-//   let filePath = path.join(
-//     __dirname,
-//     '..',
-//     '..',
-//     '..',
-//     '..',
-//     'media',
-//     'aaron25',
-//     'MALL'
-//   )
-//   // res.send(filePath);
+app.get("/", (req, res, next) => {
+  console.log(__dirname);
+  let filePath = path.join(
+    __dirname,
+    "..",
+    "..",
+    "..",
+    "..",
+    "media",
+    "aaron25",
+    "MALL"
+  );
+  // res.send(filePath);
 
-//   res.sendFile('config.txt', { root: filePath }, function (err) {
-//     if (err) {
-//       next({ error: true })
-//     } else {
-//       next()
-//     }
-//   })
+  res.sendFile("config.txt", { root: filePath }, function (err) {
+    if (err) {
+      next({ error: true });
+    } else {
+      next();
+    }
+  });
 
-//   // fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
-//   //   if (!err) {
-//   //     console.log("received data: " + data);
-//   //     res.end();
-//   //   } else {
-//   //     console.log(err);
-//   //   }
-//   // });
-// })
-// app.get('/:filename', (req, res, next) => {
-//   console.log(__dirname)
-//   let filePath = path.join(
-//     __dirname,
-//     '..',
-//     '..',
-//     '..',
-//     '..',
-//     'media',
-//     'aaron25',
-//     'MALL'
-//   )
-//   // res.send(filePath);
+  // fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
+  //   if (!err) {
+  //     console.log("received data: " + data);
+  //     res.end();
+  //   } else {
+  //     console.log(err);
+  //   }
+  // });
+});
+app.get("/:filename", (req, res, next) => {
+  console.log(__dirname);
+  let filePath = path.join(
+    __dirname,
+    "..",
+    "..",
+    "..",
+    "..",
+    "media",
+    "aaron25",
+    "MALL"
+  );
+  // res.send(filePath);
 
-//   res.sendFile(req.params.filename, { root: filePath }, function (err) {
-//     if (err) {
-//       next({ error: true })
-//     } else {
-//       next()
-//     }
-//   })
+  res.sendFile(req.params.filename, { root: filePath }, function (err) {
+    if (err) {
+      next({ error: true });
+    } else {
+      next();
+    }
+  });
 
-//   // fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
-//   //   if (!err) {
-//   //     console.log("received data: " + data);
-//   //     res.end();
-//   //   } else {
-//   //     console.log(err);
-//   //   }
-//   // });
-// })
+  // fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
+  //   if (!err) {
+  //     console.log("received data: " + data);
+  //     res.end();
+  //   } else {
+  //     console.log(err);
+  //   }
+  // });
+});
 
 var detected = false;
 console.log(detected);
